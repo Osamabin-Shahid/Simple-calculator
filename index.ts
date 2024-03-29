f90a4a4a40557ca252ae5ae6712b67437e958ba1
Osamabin-Shahid <obsmr23@gmail.com>
2024-03-29 19:40:19 +0500

    first uploaded

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter first number",
+   type: "number",
+    name: "firstNumber" 
+  },
+
+  { 
+    message: "Enter second number",
+   type: "number",
+    name: "secondNumber" 
+  },
+  
   {
     message: "Select one of the operator to perform action",
     type: "list",
